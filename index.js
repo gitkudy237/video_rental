40b9852ad2f8aa84c922f4660f0f3fa3082ae51a
Kudy <bayardkudy1@gmail.com>
2025-11-12 12:14:49 +0100

    Initialise genres and port

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
+
+const genres = [
+  { id: 1, name: "Genre1" },
+  { id: 2, name: "Genre2" },
+  { id: 3, name: "Genre3" },
+];
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
