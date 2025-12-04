e7aa23207e4c0d98311ecb28644c240fc72ccc4d
Kudy <bayardkudy1@gmail.com>
2025-12-05 00:43:02 +0100

    Create movie model

---
+++ models/movie.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Genre } = require("./genre");
+
+const genreSchema = Genre.schema;
+
+const Movie = mongoose.model(
+  "Movie",
+  mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+    genre: {
+      type: genreSchema,
+      required: true,
+    },
+    mumberInStock: {
+      type: Number,
+      default: 0,
+    },
+    dailyRate: {
+      type: Number,
+      default: 0,
+    },
+  })
+);
