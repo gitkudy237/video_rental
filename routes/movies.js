207c0d23edd13785dc6350ce3d3bbb43c4295e68
Kudy <bayardkudy1@gmail.com>
2025-12-05 01:47:49 +0100

    Implement other routes

--- routes/movies.js
+++ routes/movies.js
@@ -8,6 +8,15 @@ router.get("/", async (req, res) => {
   res.status(200).send(movies);
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const movie = await Movie.findById(id);
+
+  return movie
+    ? res.status(201).send(movie)
+    : res.status(404).send("Movie not found");
+});
+
 router.post("/", async (req, res) => {
   const { error } = Validate(req.body);
   if (error) return res.status(400).send(`Error: ${error.details[0].message}`);
@@ -15,6 +24,25 @@ router.post("/", async (req, res) => {
   const movie = new Movie(req.body);
   const result = await movie.save();
   res.status(201).send(result);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { error } = Validate(req.body);
+  if (error) return res.status(400).send(`Error: ${error.details[0].message}`);
+
+  const movie = await Movie.findByIdAndUpdate(id, req.body, { new: true });
+
+  if (!movie) return res.status(404).send("Movie not found");
+  res.status(200).send(movie);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const movie = await Movie.findByIdAndDelete(id);
+  if (!movie) return res.status(404).send("No Movie with that ID");
+
+  res.send(movie);
 });
 
 module.exports = router;
