4cb2f316f080e3e2de48f9372f5dd30035b97dba
Kudy <bayardkudy1@gmail.com>
2025-12-15 00:52:32 +0100

    Extract the routes

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 require("dotenv").config();
 const winston = require("winston");
 require("winston-mongodb");
-const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+require("./startup/routes")(app);
 
 winston.add(
   new winston.transports.File({
@@ -28,9 +22,6 @@ winston.add(
   new winston.transports.MongoDB({ db: "mongodb://localhost/vidly" })
 );
 
-const p = Promise.reject(new Error("Something failed miserably"));
-p.then(() => console.log("Something should happen"));
-
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
@@ -40,17 +31,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("Connected to mongoDB"))
   .catch((err) => console.error("Could not connect to mongoDB: ", err.message));
-
-app.use(express.json());
-
-app.use("/api/genres", genres);
-app.use("/api/customer", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- routes/genres.js
+++ routes/genres.js
@@ -6,7 +6,6 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  throw new Error("Could not get the genres.");
   const result = await Genre.find().sort({ name: 1 });
   res.send(result);
 });
