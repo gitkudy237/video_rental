a80039b4478be765b48742f9f0b84bb93c89cdc3
Kudy <bayardkudy1@gmail.com>
2025-12-15 02:46:23 +0100

    Add console logger

--- startup/logging.js
+++ startup/logging.js
@@ -3,14 +3,29 @@ require("winston-mongodb");
 
 module.exports = function () {
   winston.add(
-    new winston.transports.File({
-      filename: "uncaught-exceptions.log",
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message, stack }) => {
+          return `${timestamp} [${level}]: ${stack || message}`;
+        })
+      ),
       handleExceptions: true,
       handleRejections: true,
     })
   );
 
-  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(
+    new winston.transports.File({
+      filename: "logs/uncaught-exceptions.log",
+      handleExceptions: true,
+      handleRejections: true,
+    })
+  );
+
+  winston.add(new winston.transports.File({ filename: "logs/app.log" }));
   winston.add(
     new winston.transports.MongoDB({ db: "mongodb://localhost/vidly" })
   );
